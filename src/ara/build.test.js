fd8599d62be90e5298b778e92b0bae272737d4e1
Young <navskh@naver.com>
2024-02-26 22:13:06 +0900

    wip : builder Test 만드는 중

--- src/ara/build.test.js
+++ src/ara/build.test.js
@@ -1,5 +1,6 @@
 const { accessUrl } = require('../service/use-puppet');
 const { wait } = require('../utils/my-puppet');
+const { builderTestCase, builderTest } = require('./builderType');
 
 describe('폼 생성 테스트', () => {
   let page = null;
@@ -20,70 +21,42 @@ describe('폼 생성 테스트', () => {
     await wait(700);
   });
   test('"텍스트" 선택 시 최하단에 텍스트 컴포넌트가 추가된다.', async () => {
-    const layer = await page.$('.side-panel.open');
-    const textBtn = await layer.$('.layer-item-name');
-    await textBtn.click();
-    const elements = await page.$$('.form'); // 클래스 이름으로 모든 엘리먼트 선택
-    const lastElement = elements[elements.length - 1]; // 마지막 엘리먼트 선택
-    const textValue = await lastElement.evaluate(node => node.textContent);
-    const nodeType = await lastElement.evaluate(node => node.nodeName);
-    expect(textValue).toBe('텍스트 영역');
-    expect(nodeType).toBe('DIV');
+    const { index, containTag, text } = builderTestCase['텍스트'];
+    const { textValue, contains } = await builderTest(page, index, containTag);
+    expect(textValue).toBe(text);
+    expect(contains).toBe(true);
   });
   test('"제목(대)" 클릭 시 최하단에 대제목이 추가된다.', async () => {
-    const layer = await page.$('.side-panel.open');
-    const titleBtn = await layer.$(
-      '#root > div > div.contents > div.side-panel.open > ul:nth-child(3) > li:nth-child(2)',
-    );
-    await titleBtn.click();
-    const elements = await page.$$('.form'); // 클래스 이름으로 모든 엘리먼트 선택
-    const lastElement = elements[elements.length - 1]; // 마지막 엘리먼트 선택
-    const textValue = await lastElement.evaluate(node => node.textContent);
-    const containsH1 = await lastElement.evaluate(element => {
-      return element.querySelector('h1') !== null;
-    });
-    expect(textValue).toBe('대제목 영역');
-    expect(containsH1).toBe(true);
+    const { index, containTag, text } = builderTestCase['제목(대)'];
+    const { textValue, contains } = await builderTest(page, index, containTag);
+    expect(textValue).toBe(text);
+    expect(contains).toBe(true);
   });
   test('"제목(소)" 클릭 시 최하단에 소제목이 추가된다.', async () => {
-    const layer = await page.$('.side-panel.open');
-    const titleBtn = await layer.$(
-      '#root > div > div.contents > div.side-panel.open > ul:nth-child(3) > li:nth-child(3)',
-    );
-    await titleBtn.click();
-    const elements = await page.$$('.form'); // 클래스 이름으로 모든 엘리먼트 선택
-    const lastElement = elements[elements.length - 1]; // 마지막 엘리먼트 선택
-    const textValue = await lastElement.evaluate(node => node.textContent);
-    const containsH3 = await lastElement.evaluate(element => {
-      return element.querySelector('h3') !== null;
-    });
-    expect(textValue).toBe('소제목 영역');
-    expect(containsH3).toBe(true);
+    const { index, containTag, text } = builderTestCase['제목(소)'];
+    const { textValue, contains } = await builderTest(page, index, containTag);
+    expect(textValue).toBe(text);
+    expect(contains).toBe(true);
   });
   test('"표" 클릭 시 최하단에 표가 추가된다.', async () => {
-    const layer = await page.$('.side-panel.open');
-    const titleBtn = await layer.$(
-      '#root > div > div.contents > div.side-panel.open > ul:nth-child(3) > li:nth-child(4)',
-    );
-    await titleBtn.click();
-    const elements = await page.$$('.form'); // 클래스 이름으로 모든 엘리먼트 선택
-    const lastElement = elements[elements.length - 1]; // 마지막 엘리먼트 선택
-    const containTable = await lastElement.evaluate(element => {
-      return element.querySelector('table') !== null;
-    });
-    expect(containTable).toBe(true);
+    const { index, containTag, text } = builderTestCase['표'];
+    const { contains } = await builderTest(page, index, containTag);
+    expect(contains).toBe(true);
   });
   test('"날짜" 클릭 시 최하단에 날짜가 추가된다.', async () => {
-    const layer = await page.$('.side-panel.open');
-    const titleBtn = await layer.$(
-      '#root > div > div.contents > div.side-panel.open > ul:nth-child(3) > li:nth-child(5)',
-    );
-    await titleBtn.click();
-    const elements = await page.$$('.form'); // 클래스 이름으로 모든 엘리먼트 선택
-    const lastElement = elements[elements.length - 1]; // 마지막 엘리먼트 선택
-    const containTable = await lastElement.evaluate(element => {
-      return element.querySelector('table') !== null;
-    });
-    expect(containTable).toBe(true);
+    const { index, containTag } = builderTestCase['날짜'];
+    const { contains } = await builderTest(page, index, containTag);
+    expect(contains).toBe(true);
   });
+  test.skip("'이미지'를 선택하면 최하단에 이미지가 추가된다.", async () => {});
+  test("'주관식항목' 을 선택하면 최하단에 주관식항목이 추가된다.", async () => {});
+  test("'객관식항목' 을 선택하면 최하단에 객관식항목이 추가된다.");
+  test("'이름' 을 선택하면 최하단에 이름이 추가된다.");
+  test("'연락처' 를 선택하면 최하단에 연락처가 추가된다.");
+  test("'이메일'를 선택하면 최하단에 이메일이 추가된다.");
+  test("'생년월일'을 선택하면 최하단에 생년월일이 추가된다.");
+  test("'출신고교'를 선택하면 최하단에 출신고교가 추가된다.");
+  test("'성별'을 선택하면 최하단에 성별이 추가된다.");
+  test("'주소'를 선택하면 최하단에 주소가 추가된다.");
+  test("'버튼항목'을 선택하면 최하단에 체크박스항목이 추가된다.");
 });
--- src/ara/builderType.js
+++ src/ara/builderType.js
@@ -1,4 +1,4 @@
-async function builderTest(index, containTag, text) {
+async function builderTest(page, index, containTag) {
   const layer = await page.$('.side-panel.open');
   const sidebarItem = await layer.$(
     `#root > div > div.contents > div.side-panel.open > ul:nth-child(3) > li:nth-child(${index})`,
@@ -8,35 +8,45 @@ async function builderTest(index, containTag, text) {
   const lastElement = elements[elements.length - 1]; // 마지막 엘리먼트 선택
 
   const textValue = await lastElement.evaluate(node => node.textContent);
-  const contains = await lastElement.evaluate(element => {
+  const contains = await lastElement.evaluate((element, containTag) => {
     return element.querySelector(containTag) !== null;
-  });
-  expect(textValue).toBe(text);
-  expect(contains).toBe(true);
+  }, containTag);
+
+  return { textValue, contains };
 }
 
 const builderTestCase = {
   텍스트: {
-    index: 0,
+    index: 1,
     containTag: 'div',
     text: '텍스트 영역',
   },
   '제목(대)': {
-    index: 1,
+    index: 2,
     containTag: 'h1',
     text: '대제목 영역',
   },
   '제목(소)': {
-    index: 2,
+    index: 3,
     containTag: 'h3',
     text: '소제목 영역',
   },
   표: {
-    index: 3,
-    containTag: 'table',
-  },
-  날짜: {
     index: 4,
     containTag: 'table',
   },
+  날짜: {
+    index: 5,
+    containTag: '[type=date]',
+  },
+  이미지: {
+    index: 6,
+    containTag: 'img',
+  },
+  주관식항목: {
+    index: 7,
+    containTag: 'input',
+  },
 };
+
+module.exports = { builderTest, builderTestCase };
